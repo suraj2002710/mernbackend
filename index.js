72b23c50999709d97c50f695d4b1068458f9f1c0
suraj2002710 <113814706+suraj2002710@users.noreply.github.com>
2023-02-20 18:26:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const corsOptions = {
         "http://localhost:3000",
         "http://localhost:3001",
         "http://localhost:3002",
+      "https://delightful-semolina-c2d3de.netlify.app/",
       "*"
     ],
     credentials: true,
