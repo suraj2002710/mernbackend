6d30485d5ca2206f07187a09efe86a232c0b41f8
suraj2002710 <113814706+suraj2002710@users.noreply.github.com>
2023-09-17 15:39:28 +0530

    Update cartcontrollers.js

--- componants/cartcontrollers.js
+++ componants/cartcontrollers.js
@@ -43,8 +43,9 @@ exports.removeitem=async(req,res)=>{
 exports.updatequatnity=async(req,res)=>{
     try {
         const finddata=await model.find({id:req.query.id})
+        console.log(req.query)
         if(finddata.length){
-            console.log(req.query)
+            
             const newqty=finddata[0].quantity
             const itemid=finddata[0].id
             // console.log(itemid);
